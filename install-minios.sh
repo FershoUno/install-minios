97fd5049f0597c00a1336e1cf8922641bb80a31f
FershoUno <felix.apaza@nucleolinux.org>
2023-07-10 09:15:59 -0400

    simple installer update.

--- install-minios.sh
+++ install-minios.sh
@@ -1,183 +1,103 @@
 #!/bin/bash
 
-set -x
+#set -x
 
-# Path to MiniOS live CD
-PATH_MINIOS_LIVE_CD="/run/initramfs/memory/data"
+check_root(){
+# Verificar si se está ejecutando como root
+if [[ $(id -u) -eq 0 ]]; then
+    # Si se está ejecutando como root, ejecutar el menú principal directament
+	check_language_system
+else
+    # Si no se está ejecutando como root, solicitar autenticación de root usando pkexec y luego ejecutar el menú principal
+    if pkexec true; then
+        # Autenticación de root exitosa, ejecutar el menú principal
+        check_language_system
+    else
+        # Autenticación de root fallida, mostrar mensaje de error y salir
+        echo "No se pudo autenticar como root. El script se cerrará."
+        exit 1
+    fi
+fi
 
-# Function to handle force exit (Ctrl + C)
-force_exit() {
-    yad --info --title="$INFO" --text="$MESSAGE_TEXT_FORCE_EXIT" --no-wrap
-    exit 0
 }
-
-# Trap signals to call force_exit function
-trap force_exit SIGINT SIGTERM
 
 # Function for Spanish installation
-spanish_install() {
-    # Localization strings for Spanish
-    MAIN_MENU="Menú principal"
-    MAIN_OPTION_COLUMN="Opción"
-    MAIN_TEXT="Seleccione una opción"
-    MAIN_OPTION_ONE="Seleccionar disco e instalar MiniOS"
-    EXIT="Salir"
-    SELECT_DISK_TITLE="Seleccionar disco"
-    COLUMN_DISK="Disco"
-    SELECT_DISK_TEXT="Seleccione un disco"
-    TITLE_WARNING="Advertencia"
-    TEXT_WARNING1="Desea instalar MiniOS en el disco ->"
-    TEXT_WARNING2="?\n\nAdvertencia: Se formateará y se perderán todos los datos en el disco."
-    TEXT_FORMAT_0_10="Formateando el disco /dev/$disk con tabla de partición MBR y sistema de archivos Ext4..."
-    TEXT_FORMAT_30_40="# Creando el directorio /mnt/${disk}1..."
-    TEXT_FORMAT_60_70="# Montando el disco en /mnt/${disk}1..."
-    TEXT_FORMAT_80_90="# Copiando los archivos de instalación de MiniOS a /mnt/${disk}1..."
-    TEXT_FORMAT_100="# Ejecutando el script de instalación de MiniOS..."
-    TITLE_INSTALLING="Instalando MiniOS"
-    TITLE_TEXT_INSTALLING="Iniciando la instalación de MiniOS en el disco /dev/$disk..."
-    TITLE_FINISH="Instalación completa"
-    TITLE_TEXT_FINISH1="La instalación de MiniOS en el disco"
-    TITLE_TEXT_FINISH2="se ha completado correctamente."
-    TITLE_ERROR="Error"
-    TITLE_TEXT_ERROR="Se produjo un error durante la instalación de MiniOS en el disco"
-    MESSAGE_TEXT_FORCE_EXIT="Gracias por utilizar el instalador de MiniOS. ¡Hasta luego!"
-    INFO="Información"
-    show_main_menu
+spanish_translate(){
+	# Localization strings for Spanish
+echo $welcome_es
+	check_efi_run
+	main_menu
 }
 
-# Function for English installation
-english_install() {
-    # Localization strings for English
-    MAIN_MENU="Main Menu"
-    MAIN_OPTION_COLUMN="Option"
-    MAIN_TEXT="Please select an option"
-    MAIN_OPTION_ONE="Select disk and install MiniOS"
-    EXIT="Exit"
-    SELECT_DISK_TITLE="Select Disk"
-    COLUMN_DISK="Disk"
-    SELECT_DISK_TEXT="Please select a disk"
-    TITLE_WARNING="Warning"
-    TEXT_WARNING1="Do you want to install MiniOS on disk ->"
-    TEXT_WARNING2="?\n\nWarning: This will format the disk and all data on it will be lost."
-    TEXT_FORMAT_0_10="Formatting disk /dev/$disk with MBR partition table and Ext4 file system..."
-    TEXT_FORMAT_30_40="# Creating directory /mnt/${disk}1..."
-    TEXT_FORMAT_60_70="# Mounting disk to /mnt/${disk}1..."
-    TEXT_FORMAT_80_90="# Copying MiniOS installation files to /mnt/${disk}1..."
-    TEXT_FORMAT_100="# Running MiniOS installation script..."
-    TITLE_INSTALLING="Installing MiniOS"
-    TITLE_TEXT_INSTALLING="Starting installation of MiniOS on disk /dev/$disk..."
-    TITLE_FINISH="Installation Complete"
-    TITLE_TEXT_FINISH1="Installation of MiniOS on disk"
-    TITLE_TEXT_FINISH2="has been completed successfully."
-    TITLE_ERROR="Error"
-    TITLE_TEXT_ERROR="An error occurred during the installation of MiniOS on disk"
-    MESSAGE_TEXT_FORCE_EXIT="Thank you for using the MiniOS installer. Goodbye!"
-    INFO="Information"
-    show_main_menu
+# Function for Enlgish installation
+english_translate(){
+	# Localization strings for English
+echo $welcome_en
+	check_efi_run
+	main_menu
 }
 
 # Function for Russian installation
-russian_install() {
-    # Localization strings for Russian
-    MAIN_MENU="Главное меню"
-    MAIN_OPTION_COLUMN="Опция"
-    MAIN_TEXT="Пожалуйста, выберите опцию"
-    MAIN_OPTION_ONE="Выбрать диск и установить MiniOS"
-    EXIT="Выход"
-    SELECT_DISK_TITLE="Выбрать диск"
-    COLUMN_DISK="Диск"
-    SELECT_DISK_TEXT="Пожалуйста, выберите диск"
-    TITLE_WARNING="Предупреждение"
-    TEXT_WARNING1="Хотите установить MiniOS на диск ->"
-    TEXT_WARNING2="?\n\nПредупреждение: Диск будет отформатирован, и все данные на нем будут потеряны."
-    TEXT_FORMAT_0_10="Форматирование диска /dev/$disk с таблицей разделов MBR и файловой системой Ext4..."
-    TEXT_FORMAT_30_40="# Создание каталога /mnt/${disk}1..."
-    TEXT_FORMAT_60_70="# Монтирование диска в /mnt/${disk}1..."
-    TEXT_FORMAT_80_90="# Копирование файлов установки MiniOS в /mnt/${disk}1..."
-    TEXT_FORMAT_100="# Запуск скрипта установки MiniOS..."
-    TITLE_INSTALLING="Установка MiniOS"
-    TITLE_TEXT_INSTALLING="Начало установки MiniOS на диск /dev/$disk..."
-    TITLE_FINISH="Установка завершена"
-    TITLE_TEXT_FINISH1="Установка MiniOS на диск"
-    TITLE_TEXT_FINISH2="завершена успешно."
-    TITLE_ERROR="Ошибка"
-    TITLE_TEXT_ERROR="Во время установки MiniOS на диск произошла ошибка"
-    MESSAGE_TEXT_FORCE_EXIT="Спасибо за использование установщика MiniOS. До свидания!"
-    INFO="Информация"
-    show_main_menu
+russian_translate(){
+	# Localization strings for Russian
+echo $welcome_ru
+	check_efi_run
+	main_menu
 }
 
-language_select() {
-    # Language selection dialog
-    language=$(yad --list --title="MiniOS Installer" --text="Select the installer language:" --column="Language" "English" "Español" "Русский" --height=200 --width=250 | cut -d"|" -f1)
 
-    # Execute function based on language selection
-    case $language in
-    "English")
-        english_install
-        ;;
-    "Español")
-        spanish_install
-        ;;
-    "Русский")
-        russian_install
-        ;;
-    *)
-        force_exit
-        ;;
-    esac
+# Get the system language
+check_language_system(){
+
+language_code=${LANG}
+language_code=$(echo $language_code | cut -d "_" -f 1)
+welcome_es="¡Bienvenido!"
+welcome_en="Welcome!"
+welcome_ru="Добро пожаловать!"
+
+# Check the language and display the corresponding welcome message.
+if [[ $language_code == "es" ]]; then
+    spanish_translate
+    echo $welcome_es
+elif [[ $language_code == "en" ]]; then
+    echo $welcome_en
+    english_translate
+elif [[ $language_code == "ru" ]]; then
+    echo $welcome_ru
+    russian_translate
+else
+    exit 0
+fi
+
 
 }
 
-# Function to display the main menu
-show_main_menu() {
-    CHOICE=$(yad --list --title="$MAIN_MENU" --column="$MAIN_OPTION_COLUMN" --text="$MAIN_TEXT" \
-        "$MAIN_OPTION_ONE" "$EXIT" | cut -d"|" -f1)
 
-    case $CHOICE in
-    "$MAIN_OPTION_ONE")
-        select_disk_and_install
-        ;;
-    "$EXIT")
-        force_exit
-        ;;
-    *)
-        show_main_menu
-        ;;
-    esac
+check_efi_run(){
+
+if [ -d "/sys/firmware/efi" ]; then
+  mensaje_efi="Estás iniciando con EFI"
+else
+  mensaje_efi="Estás iniciando sin EFI"
+fi
+
 }
 
-# Function to select a disk and perform MiniOS installation
-select_disk_and_install() {
-    DISKS=$(lsblk -o NAME,SIZE -n -d -e 7,11 | awk '{print $1 "(" $2 ")"}')
+list_disks(){
 
-    SELECTED_DISK=$(yad --list --title="$SELECT_DISK_TITLE" --column="$COLUMN_DISK" --text="$SELECT_DISK_TEXT" ${DISKS} | cut -d"|" -f1)
+# Retreive the list of hard disk devices
+devices_disk=$(lsblk -o NAME,SIZE -n -d -e 7,11 | awk '{print $1 "(" $2 ")"}')
+devices_disk=$(echo $devices_disk | tr ' ' '!')
+filesystem="Ext4!Fat32"
+device_bootloader=$(lsblk -o NAME -n -d -e 7,11 | awk '{print $1}')
+device_bootloader=$(echo $device_bootloader | tr ' ' '!')
 
-    if [ -z "$SELECTED_DISK" ]; then
-        show_main_menu
-    else
-
-        yad --question --title="$TITLE_WARNING" --text="$TEXT_WARNING1 /dev/$SELECTED_DISK $TEXT_WARNING2" --no-wrap
-        SELECTED_DISK=$(echo $SELECTED_DISK | cut -d "(" -f 1)
-        case $? in
-        0)
-
-            format_and_install_minios "$SELECTED_DISK"
-            ;;
-        1)
-            show_main_menu
-            ;;
-        3)
-        trap force_exit SIGINT SIGTERM
-
-        esac
-    fi
 }
+###################################################################################3
 
 format_and_install_minios() {
     local disk=$1
-    echo $disk
-
+#    echo $disk
     (
         echo "0" # Initial value of the progress bar
         echo "$TEXT_FORMAT_0_10"
@@ -185,7 +105,7 @@ format_and_install_minios() {
 
         # Format the disk with MBR partition table and Ext4 file system
         echo -e "o\nn\np\n1\n\n\nt\n83\nw" | fdisk /dev/$disk
-        mkfs.ext4 /dev/${disk}1 -L "MiniOS System"
+        mkfs.$FORMAT_FILESYSTEM /dev/${disk}1 -L "MiniOS System"
 
         echo "30"
         echo "$TEXT_FORMAT_30_40"
@@ -215,6 +135,7 @@ format_and_install_minios() {
 
     if [ $? -eq 0 ]; then
         yad --info --title="$TITLE_FINISH" --text="$TITLE_TEXT_FINISH1 /dev/$disk $TITLE_TEXT_FINISH2" --no-wrap
+        force_exit
     else
         yad --error --title="$TITLE_ERROR" --text="$TITLE_TEXT_ERROR /dev/$disk ."
     fi
@@ -222,8 +143,49 @@ format_and_install_minios() {
     show_main_menu
 }
 
-# Language menu
-language_select
+##############################################################################################
 
-# Display the main menu
-show_main_menu
+# Main Menu
+main_menu() {
+    list_disks
+    selection=$(yad --form \
+        --title="Menú de opciones" \
+        --text="Seleccione las opciones de instalación:\n${mensaje_efi}" \
+        --width=300 --height=200 \
+       --buttons-layout="center" \
+        --on-top \
+        --form \
+        --field="Seleccionar dispositivo:CB" "${devices_disk}" \
+        --field="Seleccionar sistema de archivos:CB" "${filesystem}" \
+        --button="Instalar":0 \
+	--button="Cancel":1 \
+	--button="Refresh Disks":2 )
+
+function_button "$selection"
+    # Retrieve the selected values
+    selected_device_disk=$(echo "$selection" | cut -d"|" -f1)
+    selected_filesystem=$(echo "$selection" | cut -d"|" -f2)
+
+function_button "$selection"
+}
+
+function_button(){
+
+local button_selected=$?
+case $button_selected in
+	0)
+	echo $selected_device_disk
+	echo $selected_filesystem
+	;;
+	1)
+	echo "cancelar"
+	exit 0
+	;;
+	2)
+	echo "Refresh disk"
+	main_menu
+	;;
+esac
+}
+
+check_root
